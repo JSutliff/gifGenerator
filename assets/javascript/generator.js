d585af6c1ad050b64009037d6b4173f41bd07a8b
Jason Sutliff <jbopsut@gmail.com>
2018-08-13 16:37:13 -0700

    general pseudo code written and empty divs added

--- assets/javascript/generator.js
+++ assets/javascript/generator.js
@@ -1,1 +1,15 @@
-console.log('test link');
+//create array of theme strings
+
+
+//for each theme(elem) create a button on the page 
+
+  //if button clicked make API call to giphy api returning 10 results with rating 
+
+//intake user input in addUserButton form 
+
+  //create new button with user input that generates giphy button and displays in buttonsGoHere div
+
+
+//if giph image clicked, video plays, if clicked video stops
+ //var isPlaying
+ //
